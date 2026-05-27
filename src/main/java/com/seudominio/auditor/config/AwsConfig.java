6099c4b287240410e8a74ad77be9ef509160a1ae
pedrojoaofreitasdasilvafacul-cloud <pedrojoaofreitasdasilvafacul@gmail.com>
2026-05-26 22:27:33 -0300

    Corrigindo variaveis de ambiente

--- src/main/java/com/seudominio/auditor/config/AwsConfig.java
+++ src/main/java/com/seudominio/auditor/config/AwsConfig.java
@@ -2,8 +2,7 @@ package com.seudominio.auditor.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
@@ -12,14 +11,9 @@ public class AwsConfig {
 
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(
-            "AKIAVW6VLGVUDIGH33PW",
-            "YTnRtvX/3gHZhCZZZPSLfi+4m5NltZzuxVE3iDdp"
-        );
-        
         return SqsAsyncClient.builder()
                 .region(Region.US_EAST_1)
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
